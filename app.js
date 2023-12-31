3e1b74c79392f8163a0067bcb0abcf4d7e0761c3
Yana-Dyachok <annetart1994@gmail.com>
2023-12-31 16:55:27 +0200

    fix: change classes names in single style according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput=document.querySelector(".add-tasks__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("task__list--todo");//ul of #task__list--todo
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder=document.getElementById("task__list--completed");//task__list--completed
 
 
 //New task list item
@@ -25,10 +25,10 @@ var createNewTaskElement=function(taskString){
     var label=document.createElement("label");//label
     //input (text)
     var editInput=document.createElement("input");//text
-    //button.edit
+    //task__btn--edit
     var editButton=document.createElement("button");//edit button
 
-    //button.delete
+    //task__btn--delete
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
@@ -42,9 +42,9 @@ var createNewTaskElement=function(taskString){
     editInput.className = "input__text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className = "task__btn--edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="task__btn--delete btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "task__remove-img";
     deleteButton.appendChild(deleteButtonImg);
@@ -86,7 +86,7 @@ var editTask=function(){
 
     var editInput = listItem.querySelector(".input__text");
     var label = listItem.querySelector(".task__label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn = listItem.querySelector(".task__btn--edit");
     var containsClass=listItem.classList.contains("task__edit-mode");
     //If class of the parent is .task__edit-mode
     if(containsClass){
@@ -121,7 +121,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
     console.log("Complete Task...");
 
-    //Append the task list item to the #completed-tasks
+    //Append the task list item to the #task__list--completed
     var listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
@@ -158,8 +158,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox = taskListItem.querySelector(".input__checkbox");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".task__btn--edit");
+    var deleteButton=taskListItem.querySelector(".task__btn--delete");
 
 
     //Bind editTask to edit button.
