3b62976a8f389609856971a02d5b1d90540f2ed7
david-anav <art_migalev@yahoo.com>
2024-01-04 16:25:34 +0300

    refactor:  src to img and links

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.className="delete";
-    deleteButtonImg.src='../assets/images/svg/remove.svg';
+    deleteButtonImg.src='./assets/images/svg/remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
 
