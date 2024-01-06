3422a880918836efb053b9a51ab043771b4ca65e
jeweler23 <lopatin.lopatin2011@gmail.com>
2024-01-06 20:38:06 +0300

    fix: add class deleteButtonImg

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.className="delete";
+    deleteButtonImg.className="delete-image";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
