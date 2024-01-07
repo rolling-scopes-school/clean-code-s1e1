c8e750a8c1afe3334155f9ddbcdd332d23e81e9b
dashaborzova <dashaborzova@yandex.ru>
2024-01-08 01:19:45 +0300

    refactor: apply single style for selectors naming

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById('new-task');//Add a new task.
 var addButton=document.getElementsByTagName('button')[0];//first button
-var incompleteTaskHolder=document.getElementById('incompleteTasks');//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
+var incompleteTaskHolder=document.getElementById('incomplete-task');//ul of #incomplete-task
+var completedTasksHolder=document.getElementById('completed-task');//completed-task
 
 
 //New task list item
@@ -83,11 +83,11 @@ var editTask=function(){
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector('label');
   var editBtn=listItem.querySelector('.edit');
-  var containsClass=listItem.classList.contains('editMode');
-  //If class of the parent is .editmode
+  var containsClass=listItem.classList.contains('edit-mode');
+  //If class of the parent is .edit-mode
   if(containsClass){
 
-    //switch to .editmode
+    //switch to .edit-mode
     //label becomes the inputs value.
     label.innerText=editInput.value;
     editBtn.innerText='Edit';
@@ -96,8 +96,8 @@ var editTask=function(){
     editBtn.innerText='Save';
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle('editMode');
+  //toggle .edit-mode on the parent.
+  listItem.classList.toggle('edit-mode');
 };
 
 
@@ -116,7 +116,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
   console.log('Complete Task...');
 
-  //Append the task list item to the #completed-tasks
+  //Append the task list item to the #completed-task
   var listItem=this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
@@ -128,7 +128,7 @@ var taskIncomplete=function(){
   console.log('Incomplete Task...');
 //Mark task as incomplete.
 //When the checkbox is unchecked
-//Append the task list item to the #incompleteTasks.
+//Append the task list item to the #incomplete-task.
   var listItem=this.parentNode;
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem,taskCompleted);
