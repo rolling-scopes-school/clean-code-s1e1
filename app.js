e2bd5f4f11b0c6723f379262658eb752ba4d5c98
Vlada Yakowleva <116439108+SchizoVIv@users.noreply.github.com>
2024-01-09 03:05:51 +0200

    feat: add alert-massage
    feat: add alert-massage

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
 
+alert("Забыла заменить ссылку, по дефолту загрузила ghpages. Вот нужная https://github.com/rolling-scopes-school/clean-code-s1e1/pull/50");
+
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
 //Break things down into smaller steps and take each step at a time.
