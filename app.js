754705fca76fafe1c01b1849e5cef3203687001a
Dasha <girl.is.anime20@gmail.com>
2024-01-11 19:28:52 +0300

    fix(no rule): bug in the application 2

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add('main__item')
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
