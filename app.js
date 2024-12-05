4be5bfed600bf8189502167979d489b538fff25f
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2024-12-05 12:35:01 +0300

    refactor(basic-3.3): use names that are as long as necessary

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
   editInput.className="task";  
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";  
-  deleteButton.className="delete";
+  deleteButton.className="del";
   deleteButtonImg.src="./remove.svg";
   deleteButton.appendChild(deleteButtonImg);  
   //and appending.
@@ -149,7 +149,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
   var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var deleteButton=taskListItem.querySelector("button.del");
 
 
   //Bind editTask to edit button.
