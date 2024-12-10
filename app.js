6a6ab48dfdf70f44b53b4682361caf52f22f7eb7
Artem Sosnovich <kuvsh1noffpavel@mail.ru>
2024-12-10 08:22:38 +0300

    refactor(extended-1.2): Add alternative images content.

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='remove-icon';
     deleteButton.appendChild(deleteButtonImg);
 
 
