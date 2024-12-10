7a32e242fa548165157142f6fbb07d3d2c1df628
Mulwor <adigezalli@yandex.ru>
2024-12-10 18:09:37 +0400

    fix: add alt attribute according by rule: 1.2. Media alternatives: implemented in the whole project.

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='button image for remove item';
     deleteButton.appendChild(deleteButtonImg);
 
 
