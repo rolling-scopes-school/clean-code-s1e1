30bf3919d856346388fdc742b7961d0c47e9dfc2
Mulwor <adigezalli@yandex.ru>
2024-12-11 13:45:32 +0400

    refactor: keep the name of classes according by rule 3.3. Keep the names of identifiers and classes concise: implemented in the whole project.

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new-task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incompl-tasks");//ul of #incompl-tasks
+var completedTasksHolder=document.getElementById("compl-tasks");//compl-tasks
 
 
 //New task list item
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit-btn";
 
-    deleteButton.className="delete-btn";
+    deleteButton.className="del-btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.alt='button image for remove item';
     deleteButton.appendChild(deleteButtonImg);
@@ -158,7 +158,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
     var editButton=taskListItem.querySelector("button.edit-btn");
-    var deleteButton=taskListItem.querySelector("button.delete-btn");
+    var deleteButton=taskListItem.querySelector("button.del-btn");
 
 
     //Bind editTask to edit button.
