3c2ed4b606c13e2aa72d7cf4e94ea1299f8fc3ef
Ivan <ivanbuglac@gmail.com>
2024-12-12 13:07:00 +0300

    init: start clean-code-s1e1 task

--- app.js
+++ app.js
@@ -185,11 +185,6 @@ for (var i=0; i<completedTasksHolder.children.length;i++){
     bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 }
 
-
-
-
 // Issues with usability don't get seen until they are in front of a human tester.
-
 //prevent creation of empty tasks.
-
 //Change edit to save when you are in edit mode.
