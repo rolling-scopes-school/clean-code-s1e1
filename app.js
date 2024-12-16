d1d1f368245d3fd8eb1ab43ce4a2e3b8ae421f94
pashaBy94 <pascom_by@mail.ru>
2024-12-16 18:59:06 +0300

    refactor: update semantic and BEM according to rules in html-and-css.md

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ var createNewTaskElement = function (taskString) {
   editInput.className = "task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "todo__button--edit";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "todo__button--delete";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -76,7 +76,7 @@ var editTask = function () {
 
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
+  var editBtn = listItem.querySelector(".todo__button--edit");
   var containsClass = listItem.classList.contains("editMode");
   //If class of the parent is .editmode
   if (containsClass) {
@@ -138,8 +138,8 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var editButton = taskListItem.querySelector("button.todo__button--edit");
+  var deleteButton = taskListItem.querySelector("button.todo__button--delete");
 
   //Bind editTask to edit button.
   editButton.onclick = editTask;
