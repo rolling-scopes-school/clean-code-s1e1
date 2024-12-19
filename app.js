23e303e241f779404fb13bc4d82993e751e16198
Obukhovich Nelli <obukhovichnelly@gmail.com>
2024-12-19 17:25:20 +0300

    refactor: delete extra whitespace to rule 5 in generic-principles.md

--- app.js
+++ app.js
@@ -12,13 +12,11 @@ const createNewTaskElement=function(taskString){
     const editInput=document.createElement("input");//text
     const editButton=document.createElement("button");//edit button
 
-
     const deleteButton=document.createElement("button");//delete button
     const deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
     label.className='task';
-
 
     checkBox.type="checkbox";
     editInput.type="text";
@@ -31,8 +29,6 @@ const createNewTaskElement=function(taskString){
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
-
- 
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
     listItem.appendChild(editInput);
@@ -41,31 +37,21 @@ const createNewTaskElement=function(taskString){
     return listItem;
 }
 
-
-
 const addTask=function(){
     console.log("Add Task...");
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     const listItem=createNewTaskElement(taskInput.value);
-
-
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
-
     taskInput.value="";
-
 }
 
 //Edit an existing task.
-
 const editTask=function(){
     console.log("Edit Task...");
     console.log("Change 'edit' to 'save'");
-
-
     const listItem=this.parentNode;
-
     const editInput=listItem.querySelector('input[type=text]');
     const label=listItem.querySelector("label");
     const editBtn=listItem.querySelector(".edit");
@@ -84,11 +70,9 @@ const editTask=function(){
     listItem.classList.toggle("editMode");
 };
 
-
 //Delete task.
 const deleteTask=function(){
     console.log("Delete Task...");
-
     const listItem=this.parentNode;
     const ul=listItem.parentNode;
     //Remove the parent list item from the ul.
@@ -96,42 +80,31 @@ const deleteTask=function(){
 
 }
 
-
 //Mark task completed
 const taskCompleted=function(){
     console.log("Complete Task...");
-
-    
     const listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
 
 }
 
-
 const taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
-    
     const listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
 }
 
-
-
 const ajaxRequest=function(){
     console.log("AJAX Request");
 }
-
-
-
 
 //Set the click handler to the addTask function.
 addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
-
 
 const bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
@@ -139,25 +112,16 @@ const bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 const checkBox=taskListItem.querySelector("input[type=checkbox]");
 const editButton=taskListItem.querySelector("button.edit");
 const deleteButton=taskListItem.querySelector("button.delete");
-
-
-
     editButton.onclick=editTask;
-
     deleteButton.onclick=deleteTask;
-
     checkBox.onchange=checkBoxEventHandler;
 }
-
 
 for (let i=0; i<incompleteTaskHolder.children.length;i++){
 
     //bind events to list items chldren(tasksCompleted)
     bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
 }
-
-
-
 
 //cycle over completedTasksHolder ul list items
 for (let i=0; i<completedTasksHolder.children.length;i++){
